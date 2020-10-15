323296856a490c6d192c77da8db35c30d28dba5d
Denis-coder-star <70233191+Denis-coder-star@users.noreply.github.com>
2020-10-16 01:46:12 +0500

    site

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function(event){
+	$('.payment-window').click(function(event){
+		$('.payment-down').toggleClass('payment-down-active');
+		$('.payment-window').toggleClass('payment-window-active');
+		$('.strelka').toggleClass('strelka-active');
+	});
+	$('.down-one').click(function(event){
+		$('.rep').text('Сбербанк');
+	});
+	$('.down-two').click(function(event){
+		$('.rep').text('Киви');
+	});
+	$('.down-three').click(function(event){
+		$('.rep').text('Payeer');
+	});
+})
